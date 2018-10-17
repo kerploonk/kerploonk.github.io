721e11856b0ca3fa6e49ab1f85dfd7f90fd7386e
kerploonk <aronflavio@gmail.com>
2018-10-17 11:06:30 +0000

    Vinna á P2 hafin

---
+++ P2-Andlit/sketch.js
@@ -0,0 +1,27 @@
+// Hér kemur kóðinn þinn:
+function setup(){
+  var eyeSize = 20;
+  var faceWidth = 100;
+  var faceHeight = 150;
+  var x = 150;
+  var y = 150;
+
+  function setup(){
+    createCanvas (300,300);
+  }
+
+  function draw(){
+    background(180);
+    ellipse(x, y, faceWidth, faceHeight);
+
+    //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
+
+  }
+
+  function mousePressed(){
+    //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
+    faceWidth  = random(75,  150);
+    faceHeight = random(100, 200);
+    eyeSize    = random(10,  30);
+  }
+}
