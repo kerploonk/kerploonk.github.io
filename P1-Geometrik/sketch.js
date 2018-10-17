aa0d58d872c7145d787a511d055d52c05679d63d
kerploonk <aronflavio@gmail.com>
2018-10-17 09:56:29 +0000

    Verkefni P1 hafið

---
+++ P1-Geometrik/sketch.js
@@ -0,0 +1,14 @@
+// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(1355,654);
+    background(0,255,255);
+    fill(166);
+    ellipse(650,300,250,250);
+    fill(0);
+    textSize(20);
+    text("Halló heimur",250,350);
+    fill(255,60,7);
+    rect(200,200,10,10);
+    fill(0,220,220);
+    ellipse(300,100,100,100);  
+}
