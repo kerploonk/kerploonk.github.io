6820a85bc72c193ef46d28811351012bab58eeb9
kerploonk <aronflavio@gmail.com>
2018-10-17 10:30:55 +0000

    P1 klárað

--- P1-Geometrik/sketch.js
+++ P1-Geometrik/sketch.js
@@ -1,14 +1,22 @@
 // Hér kemur kóðinn þinn:
 function setup(){
-    createCanvas(1355,654);
-    background(0,255,255);
-    fill(166);
-    ellipse(650,300,250,250);
-    fill(0);
-    textSize(20);
-    text("Halló heimur",250,350);
-    fill(255,60,7);
-    rect(200,200,10,10);
-    fill(0,220,220);
-    ellipse(300,100,100,100);  
+    createCanvas(600,600);
+    background(0,0,0);
+    fill(222,12,12);
+    rect(0,0,255,325);
+    fill(250,250,250);
+    rect(275,0,330,325);
+    fill(250,250,250);
+    rect(0,350,255,400);
+    fill(250,250,250)
+    rect(275,350,280,235)
+    fill(15,24,219)
+    rect(560,350,40,100)
+    fill(250,250,250)
+    rect(560,460,40,150)
+    fill(244,240,7)
+    rect(275,590,155,10)
+    fill(53,53,43)
+    rect(435,590,120,10)
+
 }
