e8d4ff360c042a1eb8153567de4f352fefff0afc
kerploonk <aronflavio@gmail.com>
2018-10-24 10:14:16 +0000

    P2 klárað

--- P2-Andlit/sketch.js
+++ P2-Andlit/sketch.js
@@ -1,18 +1,48 @@
 // Hér kemur kóðinn þinn:
-function setup(){
-  var eyeSize = 20;
-  var faceWidth = 100;
-  var faceHeight = 150;
-  var x = 150;
-  var y = 150;
+  var x = 100;
+  var y = 100;
+  var antennaHeightL = 40;
+  var antennaHeightR = 40;
+  EyeHeight = 20;
+  EyeWidth = 20;
+  MouthHeight = 25;
+  MouthWidth = 25;
 
   function setup(){
     createCanvas (300,300);
   }
 
   function draw(){
-    background(180);
-    ellipse(x, y, faceWidth, faceHeight);
+    background(211,8,8);
+    // haus
+    fill(60,223,34);
+    rect(x, y, 100,100, 20);
+
+    //loftnet
+    fill(60,223,34)
+    strokeWeight(4);
+    line(125, 100, 125, antennaHeightL);
+
+    fill(60,223,34)
+    strokeWeight(4);
+    line(175, 100, 175, antennaHeightR);
+
+    //augu
+    fill(0)
+    ellipse(x+20,y+20, EyeHeight, EyeWidth);
+
+    fill(0)
+    ellipse(x+20,y+45, EyeHeight, EyeWidth);
+
+    fill(0)
+    ellipse(x+80,y+20, EyeHeight, EyeWidth);
+
+    fill(0)
+    ellipse(x+80,y+45, EyeHeight, EyeWidth);
+
+
+    //munnur
+    ellipse(x+50,y+80, MouthHeight, MouthWidth)
 
     //.....kóði sem staðsetur augun m.v. x, y, faceWidth og faceHeight ætti að koma hér.
 
@@ -20,8 +50,10 @@ function setup(){
 
   function mousePressed(){
     //Í hvert sinn sem músinni er smellt fá breyturnar ný gildi.
-    faceWidth  = random(75,  150);
-    faceHeight = random(100, 200);
-    eyeSize    = random(10,  30);
+    antennaHeightL = random(30, 70);
+    antennaHeightR = random(30, 70);
+    EyeHeight = random(10, 20);
+    EyeWidth = random(10, 20);
+    MouthHeight = random(5,25);
+    MouthWidth = random(5,25);
   }
-}
