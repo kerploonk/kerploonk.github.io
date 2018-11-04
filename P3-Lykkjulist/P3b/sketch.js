743811622c37bf85bfffdd028e66c898090b6569
kerploonk <44092044+kerploonk@users.noreply.github.com>
2018-11-04 22:03:42 +0000

    Add files via upload
    P3b gert heima

--- P3-Lykkjulist/P3b/sketch.js
+++ P3-Lykkjulist/P3b/sketch.js
@@ -3,21 +3,14 @@
 function setup() {
   createCanvas(400,400);
   background(255, 215, 0);
-  fill(0);
   colorMode(HSB);
-  frameRate(2);
+  frameRate(3);
+  strokeWeight(3);
 }
 
-var x = 10
-var y = 10
-
-function draw(){
-
-  for(var k = 0; k < 20 ; k =k +1){
-  fill(random(200,400),75,100);
-  for(var i = 0; i < 20 ; i = i + 1){
-      ellipse(x + k*20, y + i*20, 15, 15);
-
-    }
+function draw() {
+  for(var p = 300; p > 0; p = p - 30){
+    fill(random(200,400),75,100);
+    ellipse(200,200,p);
   }
 }
