60782af6528ac9d7f426ffd677dc5576695c9fe3
kerploonk <aronflavio@gmail.com>
2018-11-07 08:50:12 +0000

    P5

---
+++ P5-Spraetur/sketch.js
@@ -0,0 +1,24 @@
+
+
+var flag;
+var hammer;
+
+function preload() {
+  flag = loadImage('Myndir/flag.png'); // https://en.wikipedia.org/wiki/File:Flag_of_the_Soviet_Union_(reverse).svg
+  hammer = loadImage('Myndir/hamar.png'); // https://de.wikipedia.org/wiki/Datei:Hammer_and_sickle.svg
+}
+
+function setup() {
+  createCanvas(600,300);
+  background(hundur);
+}
+
+function mousePressed() {
+  imageMode(CENTER);
+  image(flag,mouseX,mouseY);
+}
+
+function keyPressed() {
+  imageMode(CORNER);
+  background(hammer);
+}
