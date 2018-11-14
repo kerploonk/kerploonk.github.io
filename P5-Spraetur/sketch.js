8d7c61ac9fff426cfab624e89baea61469a885b3
kerploonk <aronflavio@gmail.com>
2018-11-14 08:43:58 +0000

    P5-2

--- P5-Spraetur/sketch.js
+++ P5-Spraetur/sketch.js
@@ -1,11 +1,11 @@
 
 
-var flag;
-var hammer;
+var horse;
+var hestur;
 
 function preload() {
   horse = loadImage('Myndir/horse.png'); //https://opengameart.org/content/animal-pack-redux
-  hestur = loadImage('Myndir/hestur.png');
+  hestur = loadImage('Myndir/hestur.jpeg');
 
 function setup() {
   createCanvas(1280,720);
@@ -14,10 +14,11 @@ function setup() {
 
 function mousePressed() {
   imageMode(CENTER);
-  image(horse,ouseX,mouseY);
+  image(horse,mouseX,mouseY);
 }
 
 function keyPressed() {
   imageMode(CORNER);
   background(hestur);
+}
 }
