bbe6b1c1a24cf79c710a837503a877f3ffe5942a
kerploonk <44092044+kerploonk@users.noreply.github.com>
2018-11-19 18:20:03 +0000

    Add files via upload

--- P6-Skopp/sketch.js
+++ P6-Skopp/sketch.js
@@ -1,42 +1,43 @@
-var x = 100;
-var y = 100;
-var hradiX = 5;
-var hradiY = 11;
-var boltiStaerd = 20;
+var x = 0;
+var y = 0;
+var xSpeed = 2;
+var ySpeed = 0;
+var gravity = 0.2;
 var stig = 0;
-var spadiBreidd = 150;
-var spadiThykkt = 25;
+var spadiBreidd = 100;
+var spadiThykkt = 10;
 var spadiY;
+var boltiStaerd = 20;
 
 function setup() {
-	createCanvas(600,500);
-	spadiY = height - 45;
+	createCanvas(400,600);
+  spadiY = height - 45;
 	rectMode(CENTER);
 	textFont("Courier New",16);
 	textStyle(BOLD);
 }
 
 function draw() {
-  background(255,200,0);
-	// Reikna ný hnit út frá hraða boltans:
-  x = x + hradiX;
-  y = y + hradiY;
-	// Athuga hvort boltinn snertir veggi:
-  if ((x > width-boltiStaerd/2) || (x < boltiStaerd/2))  {
-    hradiX = hradiX * -1;
+  background(255,255,150);
+  fill(100,100,random(150,255));
+  rect(x, y, 20, 20);
+  x = x + xSpeed;
+  y = y +ySpeed;
+  ySpeed = ySpeed+gravity;
+  if ((x > width) || (x < 0)) {
+    xSpeed = xSpeed * -1;
+
   }
-  if ((y > height-boltiStaerd/2) || (y < boltiStaerd)) {
-    hradiY = hradiY * -1;
+  if (y > (height-10)) {
+    ySpeed = ySpeed * -0.9;
+    y = height-15;
   }
-	// Athuga hvort bolti snertir spaða:
-	// abs() táknar algildi/tölugildi
-	if(abs(spadiY - y) < boltiStaerd/2 + spadiThykkt/2 && abs(mouseX - x) < boltiStaerd/2 + spadiBreidd/2){
-    hradiY = hradiY * -1;
+  if(abs(spadiY - y) < boltiStaerd/2 + spadiThykkt/2 && abs(mouseX - x) < boltiStaerd/2 + spadiBreidd/2 && ySpeed>0){
+    ySpeed = ySpeed * -1;
 		stig = stig + 1;
 	}
-	// Teikna boltann, spaðann og stigin
-  fill(236,36,94);
-  rect(x, y, boltiStaerd, boltiStaerd);
+
+  fill(255,0,255);
 	rect(mouseX,spadiY ,spadiBreidd,spadiThykkt);
 	fill(0);
 	text("Stig: " + stig,20,20);
